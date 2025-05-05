17dcb4eef02e3e25204046f85e276a5f6d05eeb2
Leanca Vladimir <leancavladimir@gmail.com>
2025-05-05 22:37:26 +0300

    revert

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const port = process.env.PORT ?? 4000;
 
   app.enableCors({
-    origin: true,
+    origin: [process.env.CLIENT_URL],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: 'Content-Type, Authorization, Accept, Lang',
